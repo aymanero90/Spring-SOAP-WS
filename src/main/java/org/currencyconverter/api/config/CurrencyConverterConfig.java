6ecb55f80ff0ccba157eadfd309a830362d05408
AymanIsmaeel <ayman9089@gmail.com>
2021-04-10 11:20:43 +0200

    adding authentication (username token)

--- src/main/java/org/currencyconverter/api/config/CurrencyConverterConfig.java
+++ src/main/java/org/currencyconverter/api/config/CurrencyConverterConfig.java
@@ -1,5 +1,7 @@
 package org.currencyconverter.api.config;
 
+import org.apache.wss4j.common.WSS4JConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,16 +9,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.List;
+import java.util.Properties;
 
 @Configuration
 @EnableWs
 public class CurrencyConverterConfig extends WsConfigurerAdapter {
 
     public static final String CURRENCY_CONVERTER_URL = "http://www.currencyconverter.org/api/converter";
+
+    private final String username;
+    private final String password;
+
+    public CurrencyConverterConfig(@Value("${web.service.username}") String username,
+                                   @Value("${web.service.password}") String password) {
+        this.username = username;
+        this.password = password;
+    }
 
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
@@ -41,5 +58,29 @@ public class CurrencyConverterConfig extends WsConfigurerAdapter {
     public XsdSchema getSchema() {
         return new SimpleXsdSchema(new ClassPathResource("currencyConverter.xsd"));
     }
+
+
+    @Bean
+    SimplePasswordValidationCallbackHandler securityCallbackHandler() {
+        SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        Properties users = new Properties();
+        users.setProperty(username, password);
+        callbackHandler.setUsers(users);
+        return callbackHandler;
+    }
+
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setValidationActions(WSS4JConstants.USERNAME_TOKEN_LN);
+        securityInterceptor.setValidationCallbackHandler(securityCallbackHandler());  // Handling and validation the coming username & password in SOAP-Header of request msg
+        return securityInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(securityInterceptor());
+    }
+
 
 }
